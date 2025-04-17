8142499d3a412cef9658ee234eb2a9882dde94f6
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2025-04-17 03:16:29 -0400

    feat(cli): honor XDG_CONFIG_HOME on macOS

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,22 @@ use crate::config::Config;
 use crate::error::{Error, Result};
 use crate::upload::Uploader;
 use colored::Colorize;
+use std::env;
 use std::fs;
 use std::io::IsTerminal;
 use std::io::{self, Read};
+use std::path::PathBuf;
 
 /// Default name of the configuration file.
 const CONFIG_FILE: &str = "config.toml";
+
+fn config_dir_helper() -> PathBuf {
+    let config_dir = env::var("XDG_CONFIG_HOME").map_or_else(
+        |_| dirs_next::home_dir().unwrap_or_default().join(".config"),
+        PathBuf::from,
+    );
+    config_dir.join("rustypaste")
+}
 
 /// Runs `rpaste`.
 pub fn run(args: Args) -> Result<()> {
@@ -32,6 +42,7 @@ pub fn run(args: Args) -> Result<()> {
     } else {
         for path in [
             dirs_next::home_dir().map(|p| p.join(".rustypaste").join(CONFIG_FILE)),
+            Some(config_dir_helper().join(CONFIG_FILE)),
             dirs_next::config_dir().map(|p| p.join("rustypaste").join(CONFIG_FILE)),
         ]
         .iter()
